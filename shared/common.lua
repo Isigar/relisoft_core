5df9538858b3e5b662baac4b9957ba317db13029
Stanislav Opletal <stanislav.opletal@gmail.com>
2020-06-14 15:57:58 +0200

    new entity menu, fix other things

--- shared/common.lua
+++ shared/common.lua
@@ -125,6 +125,8 @@ function deepCopy(object)
     return _copy(object)
 end
 
+exports('deepCopy',deepCopy)
+
 --- @param sourceTable table
 --- @param targetTable table
 --- @return table
@@ -225,3 +227,36 @@ function getConfig()
 end
 
 exports('getConfig',getConfig)
+
+--Taken from ESX
+function dumpTable(table, nb)
+    if nb == nil then
+        nb = 0
+    end
+
+    if type(table) == 'table' then
+        local s = ''
+        for i = 1, nb + 1, 1 do
+            s = s .. "    "
+        end
+
+        s = '{\n'
+        for k,v in pairs(table) do
+            if type(k) ~= 'number' then k = '"'..k..'"' end
+            for i = 1, nb, 1 do
+                s = s .. "    "
+            end
+            s = s .. '['..k..'] = ' .. dumpTable(v, nb + 1) .. ',\n'
+        end
+
+        for i = 1, nb, 1 do
+            s = s .. "    "
+        end
+
+        return s .. '}'
+    else
+        return tostring(table)
+    end
+end
+
+exports('dumpTable',dumpTable)
