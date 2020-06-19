8689b4351d9fa608a1fbaae1b15c80e356afb5ce
Stanislav Opletal <stanislav.opletal@gmail.com>
2020-06-19 11:51:22 +0200

    default rotation

--- config_s.lua
+++ config_s.lua
@@ -1,2 +1,2 @@
 SConfig = {}
-SConfig.DiscordWebhook = "https://discordapp.com/api/webhooks/723426642705514537/2WPt1lbAOTxcI24R0bAeKrJ-fXD7YpGRSh2-7pnWYjpxrMPlKADh7lZUmTTD5pa4Wi6y"
+SConfig.DiscordWebhook = ""
