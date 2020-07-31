9ddca32dd1dbf6b63817713cf9fd1f699e1f6c86
Stanislav Opletal <stanislav.opletal@gmail.com>
2020-07-31 12:11:25 +0200

    create object local, networked

--- client/natives/object.lua
+++ client/natives/object.lua
@@ -1,3 +1,6 @@
+NETWORKED = true
+NON_NETWORKED = false
+
 function requestModel(model,cb)
     if not HasModelLoaded(model) and IsModelInCdimage(model) then
         while not HasModelLoaded(model) do
@@ -10,17 +13,31 @@ end
 
 exports('requestModel', requestModel)
 
+---Create networked object
 function createObject(name,pos,cb)
     local model = (type(name) == 'number' and name or GetHashKey(name))
 
     requestModel(model,function()
-        local obj = CreateObject(model,pos.x,pos.y,pos.z,true,false,false)
+        local obj = CreateObject(model,pos.x,pos.y,pos.z,NETWORKED,false,false)
         SetModelAsNoLongerNeeded(model)
         cb(obj)
     end)
 end
 
 exports('createObject',createObject)
+
+---Create local object
+function createLocalObject(name,pos,cb)
+    local model = (type(name) == 'number' and name or GetHashKey(name))
+
+    requestModel(model,function()
+        local obj = CreateObject(model,pos.x,pos.y,pos.z,NON_NETWORKED,false,false)
+        SetModelAsNoLongerNeeded(model)
+        cb(obj)
+    end)
+end
+
+exports('createLocalObject',createLocalObject)
 
 function deleteObject(obj)
     if IsEntityAttached(obj) then
