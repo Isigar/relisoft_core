a69f2f6e3fc63b154fdd224324e0a47c629528f9
Isigar <info@rcore.cz>
2020-09-06 14:00:54 +0200

    lul again

--- config_s.lua
+++ config_s.lua
@@ -1,8 +1,8 @@
 SConfig = {}
-SConfig.EnableProtection = true
+SConfig.EnableProtection = false
 SConfig.InternalDetection = 2000
 SConfig.DetectAsCheater = 5000
 SConfig.DelayBetweenDropCheater = 1000
 SConfig.LastMsgDelay = 1500
-SConfig.DiscordWebhook = "https://discordapp.com/api/webhooks/726332719809691669/UDowWHNZK26SjbYs8fLEJR51Mj7-Za2gOToDBIBbobEN68a9vD9oC-f-PPVIFkBlJp7y"
-SConfig.DiscordLogWebhook = "https://discordapp.com/api/webhooks/726332719809691669/UDowWHNZK26SjbYs8fLEJR51Mj7-Za2gOToDBIBbobEN68a9vD9oC-f-PPVIFkBlJp7y" --Internal logs
+SConfig.DiscordWebhook = ""
+SConfig.DiscordLogWebhook = "" --Internal logs
