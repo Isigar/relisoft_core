41269737aa632e4a1e68bb094b29efc159dff0cf
Isigar <info@rcore.cz>
2020-11-20 13:41:49 +0100

    empty sconfig

--- config_s.lua
+++ config_s.lua
@@ -4,5 +4,5 @@ SConfig.InternalDetection = 2000
 SConfig.DetectAsCheater = 10000
 SConfig.DelayBetweenDropCheater = 2000
 SConfig.LastMsgDelay = 3500
-SConfig.DiscordWebhook = "https://discordapp.com/api/webhooks/752136392749875232/6jlhH92MaUlPLxXaRvldofkAxP0bvrzdIlRZ16tYuGVmX98Z-ZVPs4qegkrc-AVCHRov"
-SConfig.DiscordLogWebhook = "https://discordapp.com/api/webhooks/752136392749875232/6jlhH92MaUlPLxXaRvldofkAxP0bvrzdIlRZ16tYuGVmX98Z-ZVPs4qegkrc-AVCHRov" --Internal logs
+SConfig.DiscordWebhook = ""
+SConfig.DiscordLogWebhook = "" --Internal logs
